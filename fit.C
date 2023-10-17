1849a3a5a1815eace40f80c359aa94470a71bafd
Federico Mastroforti <federico.mastroforti@studio.unibo.it>
2023-10-17 13:24:18 +0200

    fit.C fixed

--- fit.C
+++ fit.C
@@ -1,20 +1,30 @@
 #include <vector>
+#include <iostream>
+#include <memory>
 
-void fit(){
-    std::ifstream in("result.txt");
-    in.open();
+void fit()
+{
+    std::ifstream infile("result.txt");
+    
     std::vector<int> results;
-    while(in>>value){
+    int value;
+    while (infile >> value)
+    {
         results.push_back(value);
     }
 
+    int bin_number = results[0];
+    double min_extreme = results[1];
+    double max_extreme = results[2];
 
-    TH1F *h = new TH1F("h", "t", result[0], result[1] , result[2]);
-    
+    TH1F *h = new TH1F("h", "t", bin_number, min_extreme, max_extreme);
+    for (int bin{1}; bin <= bin_number; ++bin)
+    {
+        int occurrence = results[1 + bin];
+        h->Fill(bin, occurrence);
+    }
 
-
-
-
-
+    TCanvas *canvas = new TCanvas("myCanvas", "My Canvas", 800, 600);
+    h->Draw("*");
 
 }
